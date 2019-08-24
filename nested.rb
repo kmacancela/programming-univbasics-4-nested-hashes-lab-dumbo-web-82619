fe50d58796c2dbc42859e0b6db16ff7ee2d3c195
Karina Macancela <karymacancela@gmail.com>
2019-08-24 15:49:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -80,7 +86,12 @@ def adding_matz
         }
      }
 
-    
+  #newElement = {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}
+  # programmer_hash.unshift(newElement)
+  # Hash[:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}].merge!(programmer_hash)
+  # {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}.merge(programmer_hash)
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  puts programmer_hash[0]
 end
 
 def changing_alan
